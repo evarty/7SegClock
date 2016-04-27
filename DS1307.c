480538c2b67aad1b3e2cbd24f0e2c4e39b439f24
Edward <evarty@gmail.com>
2016-04-27 19:37:09 -0400

    added some comments

--- DS1307.c
+++ DS1307.c
@@ -6,14 +6,15 @@
 void DS1307Init(){//Initialize the DS1307; enable clock.
 
   TWIStart();
-  TWIWrite(DS1307ADDRESS | (0 << 0));
-  TWIWrite(0x00);
-  TWIWrite(0x00);
+  TWIWrite(DS1307ADDRESS | (0 << 0));//Write to the DS1307
+  TWIWrite(0x00);//I want to write to register 0x00, so that is the first byte
+  TWIWrite(0x00);//I want all bits in register 0x00 to be 0, so 0x00 is my second byte. In particular, I want bit 7, the clock enable bit, to be 0, so that the oscillator boots up.
   TWIStop();
 
 }
 
-
+//This function takes two arguements, which register and what byte you want that register to be written
+//The interface with the DS1307 is not critical, so this function is all I really need
 void DS1307RegisterW(uint8_t reg, uint8_t data){//Write to a single register
 
     TWIStart();
@@ -24,7 +25,8 @@ void DS1307RegisterW(uint8_t reg, uint8_t data){//Write to a single register
 
 }
 
-
+//Reads from a single register
+//Again, speed is generally not an issue, so this is adequate for most applications
 uint8_t DS1307RegisterR(uint8_t reg){//Read from a single register
 
   uint8_t data = 0;
@@ -41,7 +43,13 @@ uint8_t DS1307RegisterR(uint8_t reg){//Read from a single register
 
 }
 
-void DS1307RegisterWMult(uint8_t *reg, uint8_t NumReg, uint8_t *data, uint8_t dataLength){//Write to arbitrary number of registers
+//If I do need to write multiple registers a bit faster, I can use this
+//It takes several arguements *reg is an array of bytes, where each byte is a register, NumReg is the length of this array and the number of registers you want to write
+//I have it taking NumReg because I assume the code will know how many elements it assigned to *reg, so this saves finding out the length
+//NumReg is also the number of elements in *data
+//*data is an array where each element is the data you want written to the corresponding register
+//data[i] is written to reg[i[]
+void DS1307RegisterWMult(uint8_t *reg, uint8_t NumReg, uint8_t *data){//Write to arbitrary number of registers
 
   for(uint8_t i = 0; i < NumReg; i++){
     TWIStart();
@@ -53,6 +61,8 @@ void DS1307RegisterWMult(uint8_t *reg, uint8_t NumReg, uint8_t *data, uint8_t da
 
 }
 
+//Same as the previous function, except that it reads multiple registers and stores them in data
+//The code must ensure that *data is large enough to contain all read data
 void DS1307RegisterRMult(uint8_t FirstReg, uint8_t NumReg, uint8_t *data){//Read from arbitrary number of registers
 
   TWIStart();
